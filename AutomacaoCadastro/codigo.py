12792178f211e88acb66fbe82a9062174b7acb2c
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-06-03 23:11:50 -0300

    commitando o for

--- AutomacaoCadastro/codigo.py
+++ AutomacaoCadastro/codigo.py
@@ -29,7 +29,47 @@ pyautogui.press("enter")
 time.sleep(3)
 
 # Passo 3: Importar a base de dados - pandas
-pandas.read_csv()
+tabela = pandas.read_csv("produtos.csv")
 
 # Passo 4: Cadastrar 1 produto
+
+for linha in tabela.index:  
+    pyautogui.click(x=2333, y=358)
+    time.sleep(1)
+
+    codigo = tabela.loc[linha, "codigo"]
+    pyautogui.write(codigo)
+
+    pyautogui.press("tab")
+    marca = tabela.loc[linha, "marca"]
+    pyautogui.write(marca)
+
+    pyautogui.press("tab")
+    tipo = tabela.loc[linha, "tipo"]
+    pyautogui.write(tipo)
+
+    pyautogui.press("tab")
+    categoria = tabela.loc[linha, "categoria"]
+    pyautogui.write(str(categoria))
+
+    pyautogui.press("tab")
+    preco_unitario = tabela.loc[linha, "preco_unitario"]
+    pyautogui.write(str(preco_unitario))
+
+    pyautogui.press("tab")
+    custo = tabela.loc[linha, "custo"]
+    pyautogui.write(str(custo))
+
+    pyautogui.press("tab")
+    obs = tabela.loc[linha, "obs"]
+    
+    if obs != "nan":
+        pyautogui.write(str(obs))
+    
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+    pyautogui.scroll(10000)
+    time.sleep(2)
+
 # Passo 5: Repetir para todos os produtos
