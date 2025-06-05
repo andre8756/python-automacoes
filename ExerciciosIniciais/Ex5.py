1be11a09aa41afc959fdf2d8027a091a797e17f8
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-06-04 23:42:31 -0300

    exercicio 5 concluido

--- ExerciciosIniciais/Ex5.py
+++ ExerciciosIniciais/Ex5.py
@@ -2,5 +2,10 @@ import pyautogui
 import time
 
 pyautogui.hotkey('win', 'r')
+time.sleep(2)
 pyautogui.write('cmd')
 pyautogui.press('enter')
+time.sleep(3)
+
+pyautogui.write("taskkill /f /im cmd.exe /t")
+pyautogui.press("enter")
